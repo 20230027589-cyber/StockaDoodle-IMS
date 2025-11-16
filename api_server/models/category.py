fc9d89e9a015a14258ba7611fba6b245177fa501
Gerfel Jay <144815217+stultumJay@users.noreply.github.com>
2025-11-16 16:41:28 +0800

    feat: implement Product and Category SQLAlchemy models

---
+++ api_server/models/category.py
@@ -0,0 +1,14 @@
+from api_server.app import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), unique = True, nullable = False)
+    
+    products = db.relationship('Product', backref='category', lazy=True)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
---
+++ api_server/models/product.py
@@ -0,0 +1,31 @@
+from api_server.app import db
+import base64
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120), nullable = False)
+    brand = db.Column(db.String(50))
+    price = db.Column(db.Integer, nullable = False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    stock_level = db.Column(db.Integer, default = 0)
+    min_stock_level = db.Column (db.Integer, default = 10)
+    expiration_date = db.Column(db.Date)
+    image_blob = db.Column(db.LargeBinary)
+    
+    
+    def to_dict(self, include_image = False):
+        data = {
+            "id": self.id,
+            "name": self.name,
+            "brand": self.brand or "",
+            "price": self.price,
+            "category_id": self.category_id,
+            "stock_level": self.stock_level,
+            "min_stock_level": self.min_stock_level,
+            "expiration_date": self.expiration_date.isoformat() if self.expiration_date else None
+        }
+        if include_image and self.image_blob:
+            data["image_base64"] = base64.b64encode(self.image_blob).decode('utf-8')
+        return data
